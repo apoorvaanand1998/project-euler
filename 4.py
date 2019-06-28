a3febac370d934cfdc9dd9e5e0d0aef5b6a6844c
Apoorva Anand <apoorvaanand.work@gmail.com>
2019-06-28 10:15:38 +0530

    Find largest palindrome which is product of two 3 digit numbers

---
+++ 4.py
@@ -0,0 +1,20 @@
+
+#########################################################################################################################################
+# A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99. #
+#                                                                                                                                       #
+# Find the largest palindrome made from the product of two 3-digit numbers.                                                             #
+#########################################################################################################################################
+
+def ispal(n):
+    if str(n) == str(n)[::-1]:
+        return True
+    return False
+
+res = 0
+
+for i in range (999, 900, -1):
+    for j in range (999, 900, -1):
+        if ispal(i * j) and (i * j) > res:
+            res = i * j
+
+print(res)
