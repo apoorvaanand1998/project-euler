5e8372e0ef30e3b4b9bba3ec77fc47f426fa71a8
Apoorva Anand <apoorvaanand.work@gmail.com>
2019-08-13 15:16:56 +0530

    Solve 15 using cool math trix

--- 15.py
+++ 15.py
@@ -1,3 +1,9 @@
+############################################################################################################################################################
+# Starting in the top left corner of a 2x2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner. #
+#                                                                                                                                                          #
+# How many such routes are there through a 20x20 grid?                                                                                                     #
+############################################################################################################################################################
+
 def nop(point, grid_side, d):
     if point in d:
         return d[point]
---
+++ 15i.py
@@ -0,0 +1,7 @@
+import math
+
+print(math.factorial(40)/(math.factorial(20)*math.factorial(20)))
+
+#############################################
+# https://www.mathblog.dk/project-euler-15/ #
+#############################################
