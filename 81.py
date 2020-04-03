5ee05adf05c20c6d7c6b9608be82f2fb1a540328
Apoorva Anand <apoorvaanand.work@gmail.com>
2020-04-03 23:36:17 +0530

    Solve 81 using DP, YASSS. Apparently it could've been solved by modelling it as a graph and using Dijkstra's

--- 81.py
+++ 81.py
@@ -1,82 +1,61 @@
-import math
+import copy
 
-class Node:
-    def __init__(self, val, summed, row, column):
-        self.val = val
-        self.summed = summed
-        self.row = row
-        self.column = column
-        self.left = None
-        self.up = None
-
-    def set_left(self, left_val):
-        if (self.column - 1) >= 0:
-            self.left = Node(self.val+left_val, self.summed+left_val, self.row, self.column - 1)
-            return self.left
-        return None
-
-    def set_up(self, up_val):
-        if (self.row - 1) >= 0:
-            self.up = Node(self.val+up_val, self.summed+up_val, self.row-1, self.column)
-            return self.up
-        return None
-
-    def get_row(self):
-        return self.row
-
-    def get_column(self):
-        return self.column
-
-    def get_summed(self):
-        return self.summed
-
-    def __repr__(self):
-        return 'Node({}, {}, {}, {})'.format(self.val, self.summed, self.row, self.column)
-
-graph = []
-visited = set()
-queue = []
 matrix = []
-
 with open('p081_matrix.txt') as f:
     for e in f:
         split_e = e.split(',')
         split_e[-1] = split_e[-1][:-1]
         mapped_e = list(map(int, split_e))
         matrix.append(mapped_e)
-        
-i = len(matrix)-1
-j = len(matrix[-1])-1
-seed = Node(matrix[i][j], matrix[i][j], i, j)
 
-visited.add(seed)
-queue.append(seed)
+def traversal(x, y):
+    def up(m, n):
+        if m-1 < 0:
+            return None
+        return m-1, n
+    def left(m, n):
+        if n-1 < 0:
+            return None
+        return m, n-1
 
-while queue:
-    s = queue.pop(0)
-    i = s.get_row()
-    j = s.get_column()
-    graph.append(s)
-
-    up = s.set_up(matrix[i-1][j])
-    left = s.set_left(matrix[i][j-1])
-
-    if up is not None and up not in visited:
-        visited.add(up)
-        queue.append(up)
-    if left is not None and left not in visited:
-        visited.add(left)
-        queue.append(left)
-
-min_sum = math.inf
-for e in graph:
-    if e.get_row() == 0 and e.get_column() == 0:
-        if e.get_summed() < min_sum:
-            min_sum = e.get_summed()
-print(min_sum)
-        
+    queue = [(x, y)]
+    trav_dict = {}
+    visited = set()
     
+    while (queue):
+        curr = queue.pop(0)
+        if curr in visited:
+            continue
+        visited.add(curr)
+        u = up(*curr)
+        l = left(*curr)
+        res = []
 
+        if u is not None:
+            res.append(u)
+            queue.append(u)
+        if l is not None:
+            res.append(l)
+            queue.append(l)
 
+        if res != []:
+            trav_dict[curr] = res
+
+    return trav_dict
+
+dp = copy.deepcopy(matrix)
+trav_dict = traversal(len(matrix)-1, len(matrix[-1])-1)
+visited = set()
+
+for i in trav_dict:
+    for step in trav_dict[i]:
+        if step not in visited:
+            dp[step[0]][step[1]] += dp[i[0]][i[1]]
+            visited.add(step)
+        else:
+            if (dp[i[0]][i[1]] + matrix[step[0]][step[1]]) < dp[step[0]][step[1]]:
+                dp[step[0]][step[1]] = (dp[i[0]][i[1]] + matrix[step[0]][step[1]])
+
+print(dp[0][0])
+            
         
-    
